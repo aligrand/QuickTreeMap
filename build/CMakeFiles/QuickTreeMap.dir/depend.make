8c07e3a0a96f5da775a03ad25938f5807a0f7f8e
Ali Elahi <54209629+aligrand@users.noreply.github.com>
2024-01-18 19:25:04 +0330

    Delete build directory

--- build/CMakeFiles/QuickTreeMap.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for QuickTreeMap.
-# This may be replaced when dependencies are built.
--- build/CMakeFiles/QuickTreeMap.dir/lib/quicktreemap.cpp.obj.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/QuickTreeMap.dir/lib/quicktreemap.cpp.obj: \
- E:\AliFolder\Coding\SCIENCE\QuickTreeMap\lib\quicktreemap.cpp \
- E:/AliFolder/Coding/SCIENCE/QuickTreeMap/lib/quicktreemap.hpp
